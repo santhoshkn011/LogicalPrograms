f5b86df887006a7c3f9fcc42a7e07623d0146065
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-21 02:04:44 +0530

    Added: Welcome message.

---
+++ src/com/bridgelabz/LogicalProblems.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LogicalProblems {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Logical problem Solutions.");
+    }
+}
